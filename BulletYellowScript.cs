bded173026f466b1aad1b1cc3d9a2d1a23f69899
tkgkzyk <91358587+tkgkzyk@users.noreply.github.com>
2021-10-26 21:38:59 +0900

    Add files via upload

---
+++ BulletYellowScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletYellowScript : MonoBehaviour
+{
+    private float BulletYellowSpeed = 5.0f;
+    private GameObject player;
+    private Vector3 playerPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        playerPos = player.transform.position;
+        Destroy(this.gameObject, 7.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, playerPos, Time.deltaTime * BulletYellowSpeed);
+    }
+}
