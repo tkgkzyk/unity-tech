d7daae67eecb00c2816eae39a8a390742ce03418
tkgkzyk <91358587+tkgkzyk@users.noreply.github.com>
2021-10-26 21:34:18 +0900

    Add files via upload

---
+++ EnemyYellowScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyYellowScript : MonoBehaviour
+{
+    private float YellowSpeed = 0.5f;
+    private float timer;
+    [SerializeField] private GameObject BulletYellow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, 40f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        this.transform.position -= new Vector3(0, YellowSpeed * Time.deltaTime, 0);
+
+        if (timer > 3f)
+        {
+            Instantiate(BulletYellow, this.transform.position, Quaternion.Euler(0, 0, 0));
+            timer = 0;
+        }
+    }
+}
