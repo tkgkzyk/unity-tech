c7ea3dcd010e302d65fbe042ffe8d720d70dfc14
tkgkzyk <91358587+tkgkzyk@users.noreply.github.com>
2021-10-26 21:16:25 +0900

    Add files via upload

---
+++ BulletScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    private float speed = 5.0f;
+    private int score;
+    private string[] enemies = new string[4] { "Bullet", "Red", "Yellow", "Purple"};
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = PlayerPrefs.GetInt("Score");
+        Destroy(this.gameObject, 7.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        for(int i = 0; i < enemies.Length; i++)
+        {
+            if(collision.gameObject.tag == enemies[i])
+            {
+                score += (i + 1) * 5;
+            }
+        }
+
+        if (collision.gameObject.tag != "Player")
+        {
+            PlayerPrefs.SetInt("Score", score);
+            Destroy(collision.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
