6d9aadb529f2ee6fe253f785195b26f61a0c21c4
tkgkzyk <91358587+tkgkzyk@users.noreply.github.com>
2021-10-26 12:07:36 +0900

    Add files via upload

---
+++ Ray3dScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ray3dScript : MonoBehaviour
+{
+    int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit, 1000.0f);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (hit.collider)
+            {
+                count++;
+                Debug.Log(count);
+            }
+        }
+    }
+}
