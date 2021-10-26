63799e3a71677b99bbbe6e716b67125b71a50c54
tkgkzyk <91358587+tkgkzyk@users.noreply.github.com>
2021-10-26 21:46:32 +0900

    Add files via upload

---
+++ RendaScripts/EndSceneManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndSceneManager : MonoBehaviour
+{
+    public Text LastScoreText;
+    public Text HighScoreText;
+    int LastScore;
+    int HighScore;
+
+    void Start()
+    {
+        LastScore = PlayerPrefs.GetInt("score");
+        HighScore = PlayerPrefs.GetInt("HighScore");
+
+        if (PlayerPrefs.HasKey("HighScore") == true)
+        {
+
+            if (HighScore < LastScore)
+            {
+                HighScore = LastScore;
+                PlayerPrefs.SetInt("HighScore", LastScore);
+            }
+        }
+
+        else
+        {
+            HighScore = LastScore;
+            PlayerPrefs.SetInt("HighScore", HighScore);
+        }
+
+        LastScoreText.text = LastScore.ToString();
+        HighScoreText.text = HighScore.ToString();
+    }
+
+    private void Update()
+    {
+       
+    }
+
+    public void RetryButton()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+
+}
