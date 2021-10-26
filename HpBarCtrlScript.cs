07e7a8810c4dd0624ebc694683bdcd50efa9c6bb
tkgkzyk <91358587+tkgkzyk@users.noreply.github.com>
2021-10-26 11:51:08 +0900

    Add files via upload

---
+++ HpBarCtrlScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;//ここ忘れずに
+
+public class HpBarCtrlScript : MonoBehaviour
+{
+    Slider HpBar;
+    float Hp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //スライダーを取得
+        HpBar = GameObject.Find("Slider").GetComponent<Slider>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Hp = Mathf.Clamp(Hp, 0.0f, 1.0f);
+
+        if (Input.GetKeyDown("up"))
+        {
+            Hp += 0.1f;
+        }
+
+        if(Input.GetKeyDown("down"))
+        {
+            Hp -= 0.1f;
+        }
+
+        // HPゲージに値を設定
+        HpBar.value = Hp;
+    }
+}
