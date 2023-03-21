9a373cfd8e443233df347006df66a69f49b63933
tkgkzyk <91358587+tkgkzyk@users.noreply.github.com>
2023-03-22 04:39:50 +0900

    Add files via upload

---
+++ Scripts/BlockCubeScript1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockCubeScript1 : MonoBehaviour
+{
+    public GameObject BlockCube;
+    //float Timer;
+    Vector3 BlockKeeper;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BlockKeeper = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Timer += Time.deltaTime;
+
+        //if(Timer > 2f)
+        //{
+            //Instantiate(BlockCube, this.transform.position, this.transform.rotation);
+            //Timer = 0;
+            transform.position = new Vector3(BlockKeeper.x, BlockKeeper.y, BlockKeeper.z - Mathf.Sin(Time.time) * 10.0f);
+        //}
+    }
+}
---
+++ Scripts/GoalKeeperScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalKeeperScript : MonoBehaviour
+{
+    public float speed = 10f;
+    Animator animator;
+    //int ShootNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+
+    }
+    IEnumerator Rotation()
+    {
+        yield return new WaitForSeconds(3);
+        this.transform.rotation = Quaternion.Euler(0, 90, 0);
+    }
+
+public void ClickRightLow()
+    {
+        animator.SetBool("KeeperDiveRightLow", true);
+    }
+
+    public void ClickLeftLow()
+    {
+        animator.SetBool("KeeperDiveLeftLow", true);
+    }
+
+    public void ClickRightHigh()
+    {
+        animator.SetBool("KeeperDiveRightHigh", true);
+
+        StartCoroutine("Rotation");
+    }
+
+    public void ClickLeftHigh()
+    {
+        animator.SetBool("KeeperDiveLeftHigh", true);
+
+        StartCoroutine("Rotation");
+    }
+    public void ClickCenterLow()
+    {
+        animator.SetBool("KeeperDiveCenterLow", true);
+    }
+
+    public void ClickCenterHigh()
+    {
+        animator.SetBool("KeeperDiveCenterHigh", true);
+    }
+}
